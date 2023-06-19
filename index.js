cfa640ed5e78fe3216dfbdb91989c7a12669e1d1
bk7312 <bk7312@users.noreply.github.com>
2023-06-19 22:12:49 +0800

    improved app UI and added details

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
+const $menuCheckbox = document.querySelector('#menu-checkbox')
+const $main = document.querySelector('#main')
+
 const $todoInput = document.querySelector('#todo-input')
 const $todoText = document.querySelector('#todo-text')
+
 const $clear = document.querySelector('#clear')
+const $clearDrop = document.querySelector('#clear-drop')
 
 const $todos = document.querySelector('#todos')
 const $todosPlaceholder = document.querySelector('#todos-placeholder')
@@ -9,33 +14,41 @@ const $todosList = document.querySelector('#todos-list')
 
 const $addTask = document.querySelector('#add-task')
 
-
 let todos = JSON.parse(localStorage.getItem('todos')) || []
+let disabled = false
 
+$menuCheckbox.addEventListener('change', e => {
+    if ($menuCheckbox.checked) {
+        $main.classList.add("disable-scrolling")
+        disabled = true
+    } else {
+        $main.classList.remove("disable-scrolling")
+        disabled = false
+    }
+})
 
 $addTask.addEventListener('click', e => {
-    if ($todoText.value.trim() === '') return
+    if (disabled || $todoText.value.trim() === '') return
     addTodo()
 })
 
 $todoText.addEventListener('keypress', e => {
-    if (e.key === 'Enter' && $todoText.value.trim() !== '') {
-        addTodo()
-    }
+    if (disabled || $todoText.value.trim() === '') return
+    if (e.key === 'Enter') addTodo()
 })
 
 $todosList.addEventListener('click', e => {
+    if (disabled) return
     const id = e.target.parentNode.dataset.id
     if (e.target.classList.contains('delete-btn')) {
         deleteTodo(id)
-        displayTodos()
-    } else if (e.target.classList.contains('todo-item-text')) {
+    } else if (e.target.classList.contains('todo-checkbox')) {
         toggleDone(id)
-        displayTodos()
     }
 })
 
 $clear.addEventListener('click', e => {
+    $clearDrop.open = false
     localStorage.removeItem('todos')
     todos = []
     displayTodos()
@@ -60,18 +73,21 @@ function displayTodos() {
     $todosPlaceholder.style.display = hasTodos ? 'none' : 'flex'
     $todosList.innerHTML = todos.map(todo => `
         <div class="todo-item" data-id=${todo.id}>
+            <div class="todo-checkbox">${todo.done ? "X" : ""}</div>
             <p class="todo-item-text ${todo.done && "strikethrough"}">${todo.text}</p>
-            <div class="delete-btn"></div>
+            <div class="delete-btn icon"></div>
         </div>
     `).reverse().join('')
 }
 
 function deleteTodo(id) {
     todos = todos.filter(todo => todo.id !== id)
+    displayTodos()
 }
 
 function toggleDone(id) {
     todos = todos.map(todo => (
         todo.id === id ? {...todo, done: !todo.done} : todo
     ))
+    displayTodos()
 }
